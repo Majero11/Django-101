a7f292ba518d5f262246b2463865a137a685a22e
Tinmaj <majerographics@gmail.com>
2026-05-24 22:20:55 +0100

    added the image field and tweaked the code

--- feed/views.py
+++ feed/views.py
@@ -1,9 +1,30 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView, FormView
+from .form import PostForm
+from .models import Post
 
 class HomePageView(TemplateView):
     template_name = "home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['my_thing'] = "hello world :P this is dynamic"
+        context['posts'] = Post.objects.all().order_by('-id')
         return context
+    
+class PostDetailView(DetailView):
+    template_name = "detail.html"
+    model = Post
+    
+    
+class AddPostView(FormView):
+    template_name = "new_post.html"
+    form_class = PostForm
+    success_url = "/"
+    
+    def form_valid(self, form):
+        new_object = Post.objects.create(
+            text = form.cleaned_data['text'],
+            image = form.cleaned_data['image']
+        )
+        return super().form_valid(form)
+
+    
